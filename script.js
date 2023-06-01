5870098cb0f44fff04ad04a0561cd45611e7e9aa
Analax <93753066+Analax@users.noreply.github.com>
2023-06-02 03:00:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Add a smooth scroll effect when clicking on the VR tour link
-const vrTourLink = document.querySelector('.vr-tour a');
-vrTourLink.addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent default link behavior
-  const contentSection = document.querySelector('.content');
-  contentSection.scrollIntoView({ behavior: 'smooth' });
-});
